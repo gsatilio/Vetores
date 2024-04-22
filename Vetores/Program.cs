7f432ef4c51c195dddfb7bf944a275242d0a9f7b
Guilherme <g.satilio@gmail.com>
2024-04-22 13:42:19 -0300

    Add project files.

---
+++ Vetores/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero;
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+}
+
+for(int i = 0;i < 5; i++)
+{
+    Console.WriteLine("o numero eh:" + numeros[i]);
+}
